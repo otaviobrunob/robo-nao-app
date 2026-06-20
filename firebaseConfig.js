6a107a84aa3ae012976e59e45fbdf5d33dd31f20
Otávio Bruno <otaviobrunobr@gmail.com>
2026-06-20 16:32:44 -0300

    Configuração inicial do Firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// firebaseConfig.js
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBI0qRFhFtmssnzrdASpOuCer1i3hPNsxtY",
+  authDomain: "robo-nao-app.firebaseapp.com",
+  projectId: "robo-nao-app",
+  storageBucket: "robo-nao-app.firebasestorage.app",
+  messagingSenderId: "714140902929",
+  appId: "1:714140902929:web:fb8dbb65395e04716e33b0",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
